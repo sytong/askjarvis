046df13ba476875e3ed82c282489539cd5017c01
Willie Tong <tongsinyin@gmail.com>
2015-10-11 01:58:29 +0800

    stub default_query_params in character test

--- test/character/character_test.rb
+++ test/character/character_test.rb
@@ -7,12 +7,14 @@ class AskjarvisCharacterTest < Minitest::Test
   end
 
   def test_we_have_a_hulk
-  	VCR.use_cassette('the_hulk') do
-  	  the_hulk = Askjarvis::Character.find(1009351)
-  	  assert_equal Askjarvis::Character, the_hulk.class
+    Askjarvis.stub :default_query_params, "ts=111&apikey=111&hash=111" do
+  	  VCR.use_cassette('the_hulk') do
+  	    the_hulk = Askjarvis::Character.find(1009351)
+  	    assert_equal Askjarvis::Character, the_hulk.class
 
-  	  assert_equal 1009351, the_hulk.id
-  	  assert_equal 'Hulk', the_hulk.name
+  	    assert_equal 1009351, the_hulk.id
+  	    assert_equal 'Hulk', the_hulk.name
+      end
   	end
   end
 end
